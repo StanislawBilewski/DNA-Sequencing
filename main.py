7b338c4bab6fe315b425f9392aa93e88ba0d3f00
Student inf145203 <inf145203@lab-ge-10.cs.put.poznan.pl>
2022-05-27 09:20:04 +0200

    Update

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ class Ant:
         self.initialNode = initialNode
         self.currentNode = initialNode
         self.sightMatrix = []
-        self.route = [initialNode]
+        self.route = []
+        self.route1 = [initialNode]
+        self.route2 = []
         for i in range(len(sightMatrix)):
             self.sightMatrix.append(sightMatrix[i].copy())
 
     def calcProbabilities(self, pheromones, alpha, beta):
         probabilities = []
+        temp = 0
         for i in range(len(self.sightMatrix)):
-            if i in self.route:                 # jeśli węzeł 'i' został już odwiedzony, to wtedy nie możemy go wykorzystać
+            if i in self.route1 or i in self.route2:                 # jeśli węzeł 'i' został już odwiedzony, to wtedy nie możemy go wykorzystać
                 probabilities.append(0)
                 probabilities.append(0)
 
@@ -30,15 +33,14 @@ class Ant:
                 probabilities.append(           # prawdopodobieństwo przejścia z węzła 'i' do węzła 'initialNode' (doklejanie do początku)
                     (pheromones[i][self.initialNode] ** alpha) *        # indeks w liscie 'probabilities' będzie nieparzysty
                     (self.sightMatrix[i][self.initialNode] ** beta))
-        temp = sum(probabilities)
+                temp += probabilities[-1] + probabilities[-2]
 
         if(temp == 0):
             return [0]*len(probabilities)
 
-        for i in range(len(probabilities)):
+        probabilities[0] = probabilities[0]/temp
+        for i in range(1, len(probabilities)):
             probabilities[i] = probabilities[i]/temp
-
-        for i in range(1,len(probabilities)):
             probabilities[i] += probabilities[i-1]
 
         return probabilities
@@ -49,24 +51,23 @@ class Ant:
         while temp > probabilities[nextNode]:
             nextNode += 1
 
-        if nextNode%2 == 0:
-            nextNode = nextNode//2
+        if nextNode % 2 == 0:
+            nextNode //= 2
             self.totalGain += gainMatrix[self.currentNode][nextNode]
             self.currentNode = nextNode
-            self.route.append(self.currentNode)
+            self.route1.append(self.currentNode)
         
         else:
-            nextNode = nextNode//2
+            nextNode //= 2
             self.totalGain += gainMatrix[nextNode][self.initialNode]
             self.initialNode = nextNode
-            self.route.insert(0, self.initialNode)
+            self.route2.append(self.initialNode)
 
 
     def updatePheromones(self, pheromones):
-        if(self.totalGain > 0):
-            temp = 1 - (1/self.totalGain)
-            for i in range(len(self.route)-1):
-                pheromones[self.route[i]][self.route[i+1]] += temp
+        temp = 1 - (1/self.totalGain)
+        for i in range(len(self.route)-1):
+            pheromones[self.route[i]][self.route[i+1]] += temp
 
 def getGainMatrix(inp):
     gainMatrix = []
@@ -125,34 +126,38 @@ def antColonyOptimization(
     for iteration in range(iter):
         print("iteracja:",iteration)
         ants = []
-        distance = 0
-        distances = []
-        paths=[]
+        gain = 0
+        path = []
 
         for _ in range(nOfAnts):
             ants.append(Ant(initialNode, sightMatrix))
 
+        antno=0
         for ant in ants:
+            print("ant",antno)
+            antno+=1
             while True:
                 probabilities = ant.calcProbabilities(pheromoneMatrix, alpha, beta)
                 if sum(probabilities) == 0:
                     break
                 ant.chooseNextNode(probabilities)
 
-            distances.append(ant.totalGain)
-            paths.append(ant.route)
+            for i in range(len(ant.route2) - 1, -1, -1):
+                ant.route.append(ant.route2[i])
+
+            for i in range(len(ant.route1)):
+                ant.route.append(ant.route1[i])
+
+            if(ant.totalGain > gain or gain == 0):
+                gain = ant.totalGain
+                path = ant.route
 
         vaporization(pheromoneMatrix, epsilon)
 
         for ant in ants:
             ant.updatePheromones(pheromoneMatrix)
 
-        for i in range(len(paths)):
-            if(distance > distances[i] or distance==0):
-                distance = distances[i]             # wyznaczanie długości najkrótszej ścieżki
-                shortestPath = paths[i]
-
-    return shortestPath, distance
+    return path, gain
 
 
 inp = input("Would you like to overwrite files that are already in output? (Y/N)")
@@ -184,7 +189,7 @@ for filename in listdir('inputs/'):
     pheromoneMatrix = initPheromones(len(sightMatrix))
 
     timetest = time.time()
-    path, gain = antColonyOptimization(0, sightMatrix, iter=iterations, nOfAnts=antsNumber)
+    path, gain = antColonyOptimization(len(sightMatrix)//2, sightMatrix, iter=iterations, nOfAnts=antsNumber)
     timetest = time.time() - timetest
 
     seq = ""
